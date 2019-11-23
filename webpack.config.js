b0535144f0eb346e207005eeda72adf786140538
Arkadiusz Klonowski <arkadiusz.klonowski.1995@gmail.com>
2019-11-23 01:53:00 +0100

    configuration webpack and babel

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+module.exports = {
+    entry: "./js/app.js",
+    output: {
+        filename: "out[name].js",
+        path.resolve(__dirname, "js")
+    },
+    watch: false,
+    mode: "development", //ta opcja zostanie pominięta jeżeli użyjemy npm run build
+    devtool: "source-map",
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    }
+}
