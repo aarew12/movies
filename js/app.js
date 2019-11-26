3cbbaad57433f66afb27c66142072154d792f30a
MarcinD <m4rcin.duda@gmail.com>
2019-11-26 13:53:01 +0100

    weather description

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
 let units = 'metric';   
+let lang = 'pl';
+
 const apiId = '8d1e9284c9373751f4e24ac5eb9993ce';
 const cityInput = document.forms['city'];
 const moment = require('moment');
@@ -9,7 +11,7 @@ function getWeatherByCityName(e) {
     e.preventDefault();
     let miasto = document.getElementById("enterCity").value;
     
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${miasto}&units=${units}&appid=${apiId}`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${miasto}&units=${units}&appid=${apiId}&lang=${lang}`)
         .then(resp => {
             if (resp.ok) {
                 return resp.json();
@@ -18,13 +20,15 @@ function getWeatherByCityName(e) {
             }
         })            
         .then(resp => {
-            // console.log(resp);  
+            console.log(resp);  
             document.querySelector('.weatherinfo').innerHTML = 
             `<h3>${resp.name}, ${resp.main.temp.toFixed()} °C</h3>
+            <p>${resp.weather[0].description}</p>
             <ul>
                 <li>ciśnienie: ${resp.main.pressure} hPa</li>
                 <li>wilgotność: ${resp.main.humidity} %</li>
-                <li>prędkość wiatru: ${resp.wind.speed} km/h</li>
+                <li>prędkość wiatru: ${resp.wind.speed} m/s</li>
+                <li>kierunek wiatru: ${resp.wind.deg} °</li>
                 <li>kierunek wiatru: ${resp.wind.deg} °</li>
             </ul>`;
         })
