d36a5a53ade79056446f578b557a22c5694846f7
MarcinD <m4rcin.duda@gmail.com>
2019-11-27 22:41:44 +0100

    weather api fix + icons test

--- js/app.js
+++ js/app.js
@@ -23,9 +23,11 @@ function getWeatherByCityName(e) {
             console.log(resp);  
             const sunrise = new Date(resp.sys.sunrise * 1000);
             const sunset = new Date(resp.sys.sunset * 1000);
+            let weatherIcon = `<img src='http://openweathermap.org/img/wn/${resp.weather[0].icon}@2x.png' alt='Weather status'>`;
+            let weatherIconCss = `http://openweathermap.org/img/wn/${resp.weather[0].icon}@2x.png`;
 
             document.querySelector('.weatherinfo').innerHTML = 
-            `<h3>${resp.name}, ${resp.main.temp.toFixed()} °C</h3>
+            `<h3>${resp.name}, ${resp.main.temp.toFixed()} °C ${weatherIcon}</h3>
             <p>${resp.weather[0].description}</p>
             <ul>
                 <li>ciśnienie: ${resp.main.pressure} hPa</li>
@@ -34,8 +36,11 @@ function getWeatherByCityName(e) {
                 <li>kierunek wiatru: ${resp.wind.deg} °</li>
                 <li>wschód słońca: ${sunrise.getHours()}:${sunrise.getMinutes()}:${sunrise.getSeconds()}</li>
                 <li>zachód słońca: ${sunset.getHours()}:${sunset.getMinutes()}:${sunset.getSeconds()}</li>
-                
             </ul>`;
+            
+            // // Weather Icon // 
+            document.querySelector('.weatherimg').style.backgroundImage = `url(${weatherIconCss})`;
+
         })
         .catch(error => {
             if(error.status === 404) {
--- js/bundle.js
+++ js/bundle.js
@@ -1,5 +1,7 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 let units = 'metric';   
+let lang = 'pl';
+
 const apiId = '8d1e9284c9373751f4e24ac5eb9993ce';
 const cityInput = document.forms['city'];
 const moment = require('moment');
@@ -10,7 +12,7 @@ function getWeatherByCityName(e) {
     e.preventDefault();
     let miasto = document.getElementById("enterCity").value;
     
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${miasto}&units=${units}&appid=${apiId}`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${miasto}&units=${units}&appid=${apiId}&lang=${lang}`)
         .then(resp => {
             if (resp.ok) {
                 return resp.json();
@@ -19,15 +21,27 @@ function getWeatherByCityName(e) {
             }
         })            
         .then(resp => {
-            // console.log(resp);  
+            console.log(resp);  
+            const sunrise = new Date(resp.sys.sunrise * 1000);
+            const sunset = new Date(resp.sys.sunset * 1000);
+            let weatherIcon = `<img src='http://openweathermap.org/img/wn/${resp.weather[0].icon}@2x.png' alt='Weather status'>`;
+            let weatherIconCss = `http://openweathermap.org/img/wn/${resp.weather[0].icon}@2x.png`;
+
             document.querySelector('.weatherinfo').innerHTML = 
-            `<h3>${resp.name}, ${resp.main.temp.toFixed()} °C</h3>
+            `<h3>${resp.name}, ${resp.main.temp.toFixed()} °C ${weatherIcon}</h3>
+            <p>${resp.weather[0].description}</p>
             <ul>
                 <li>ciśnienie: ${resp.main.pressure} hPa</li>
                 <li>wilgotność: ${resp.main.humidity} %</li>
-                <li>prędkość wiatru: ${resp.wind.speed} km/h</li>
+                <li>prędkość wiatru: ${resp.wind.speed} m/s</li>
                 <li>kierunek wiatru: ${resp.wind.deg} °</li>
+                <li>wschód słońca: ${sunrise.getHours()}:${sunrise.getMinutes()}:${sunrise.getSeconds()}</li>
+                <li>zachód słońca: ${sunset.getHours()}:${sunset.getMinutes()}:${sunset.getSeconds()}</li>
             </ul>`;
+            
+            // // Weather Icon // 
+            document.querySelector('.weatherimg').style.backgroundImage = `url(${weatherIconCss})`;
+
         })
         .catch(error => {
             if(error.status === 404) {
